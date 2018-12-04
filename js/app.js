a477882d813f69bb32900bd94fb6b099d690c60a
KubaMikolajczyk <qba600@gmail.com>
2018-12-04 15:38:55 +0100

    Implement hold function

--- js/app.js
+++ js/app.js
@@ -22,6 +22,26 @@ document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+
+  //add current score to user total score
+  scores[activePlayer] += roundScore;
+
+  //update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  //check if player won the Game
+  if(scores[activePlayer] >= 100){
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
+  } else{
+    //Next player
+    nextPlayer();
+  }
+})
+
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
   //1. Generate random number
@@ -39,14 +59,18 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
-    //Next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
+    nextPlayer();
   }
 });
+
+function nextPlayer(){
+  //Next player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+}
